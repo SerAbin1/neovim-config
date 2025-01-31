83d9a976833479bf50981e16add535e89423bb62
SerAbin1 <abinyt1234@gmail.com>
2025-01-31 21:57:22 +0530

    new

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,6 @@
+return {
+    {
+        "github/copilot.vim",
+        lazy = false -- Ensure it loads at startup
+    }
+}
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -4,3 +4,9 @@ vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 
 vim.g.mapleader = " "
+
+-- Map Ctrl-x to :q
+vim.keymap.set('n', '<C-x>', ':q<CR>', { noremap = true, silent = true })
+
+-- Map Ctrl-wx to :wq
+vim.keymap.set('n', '<C-w>x', ':wq<CR>', { noremap = true, silent = true })
